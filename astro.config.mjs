43b0cb8bd44a347b2809ad178c0c61bc8f59f288
shouta0715 <Myheroes0715@gmail.com>
2023-09-05 21:54:25 +0900

    blogs page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-/* eslint-disable import/no-unresolved */
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
